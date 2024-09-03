41a01f8e6dc9ed84f034f69d73b8b5242a14f5c4
litvin Vlad <v.litvin@infocom-s.ru>
2024-09-03 14:08:15 +0300

    (1C-22468)
    Разбил DatePickerMaskedWidgetAsset на два ассета, чтобы подгружать файлы из разных директорий

--- SGUInfocomAsset.php
+++ SGUInfocomAsset.php
@@ -15,21 +15,19 @@ use yii\bootstrap4\BootstrapPluginAsset;
 use yii\web\AssetBundle;
 
 /**
- * DatePickerMaskedWidgetAsset
+ * SGUInfocomAsset
  *
  */
-class DatePickerMaskedWidgetAsset extends AssetBundle
+class SGUInfocomAsset extends AssetBundle
 {
     //$sourcePath: specifies the root directory that contains the asset files in this bundle. 
-    public $sourcePath = '@bower';
+    public $sourcePath = '@vendor/sgu-infocom-official';
 
     //$depends: an array listing the names of the asset bundles that this bundle depends on, in order. 
-    public $depends = [BootstrapPluginAsset::class, SGUInfocomAsset::class]; //IS THIS AUTOMATICALLY INCLUDED IN YII2? so I don't need to add it to my project separately
+    public $depends = [BootstrapPluginAsset::class]; //IS THIS AUTOMATICALLY INCLUDED IN YII2? so I don't need to add it to my project separately
 
     public function init()
     {
-        $this->css[] = YII_DEBUG ? 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.css' : 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.standalone.min.css';
-
-        $this->js[] = YII_DEBUG ? 'bootstrap-datepicker/dist/js/bootstrap-datepicker.js' : 'bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js';
+        $this->js[] = YII_DEBUG ? 'inputmask_v3.3.11/dist/jquery.inputmask.bundle.js' : 'inputmask_v3.3.11/dist/min/jquery.inputmask.bundle.min.js';
     }
 }
